5bb4e21779049fcfb16969bd6c6a491151ee16db
tseyatsing <tseyatsing@icloud.com>
2026-01-29 15:18:43 +0800

    URDF.urdf改成相对路径

--- Software/SeaWashr-ros2-ws/build/turtlesim/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/tse-yatsing/Underwater_Cleaning_Robot/Software/SeaWashr-ros2-ws/install/turtlesim_msgs/share/turtlesim_msgs/package.sh"
